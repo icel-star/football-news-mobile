17cddd02e1ddbe248f9f75e3cb5c2129763524ab
icel-star <giselle.julia@ui.ac.id>
2025-11-12 14:44:56 +0700

    tutorial 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:football_news/screens/menu.dart';
 import 'package:flutter/material.dart';
-import 'package:football_news/screens/newslist_form.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:football_news/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +14,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+    return Provider(
+      create: (_) {
+        CookieRequest request = CookieRequest();
+        return request;
+      },
+      child: MaterialApp(
+        title: 'Football News',
+        theme: ThemeData(
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
@@ -33,7 +40,8 @@ class MyApp extends StatelessWidget {
          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
         .copyWith(secondary: Colors.blueAccent[400]),
       ),
-      home: MyHomePage(),
+      home: const LoginPage(),
+    ),
     );
   }
 }
